be0f00e23fddeb578c0a2a43548670adf4844fab
htmkhue39 <93439255+htmkhue39@users.noreply.github.com>
2022-06-09 01:49:13 +0700

    Update form_task window

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -37,8 +41,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Form task view
-
+                showFormDialog();
             }
+
+
         });
+
+    }
+
+    private void showFormDialog(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.form_task);
+
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.bg_form);
+
+        Window window = dialog.getWindow();
+        if(window == null) return;
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.width = getScreenWidth() - getScreenWidth()/15;
+        params.height = getScreenHeight() - getScreenHeight()/4;
+        window.setAttributes(params);
+
+        Button bt_cancel = findViewById(R.id.bt_cancel);
+
+        /*bt_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });*/
+
+        dialog.show();
+    }
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight() {
+        return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
 }
