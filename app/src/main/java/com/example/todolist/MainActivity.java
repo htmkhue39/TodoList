bbb4acceda2d616b8904f430f6112d855e6f192c
htmkhue39 <93439255+htmkhue39@users.noreply.github.com>
2022-06-20 22:33:55 +0700

    Create log_out, register page

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -15,19 +18,19 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button bt_form, bt_add;
-    ImageButton bt_delete;
+    ImageButton bt_menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         editText = findViewById(R.id.edit_text);
         bt_add = findViewById(R.id.bt_add);
         bt_form = findViewById(R.id.bt_form);
-        bt_delete = findViewById(R.id.bt_delete);
+        bt_menu = findViewById(R.id.bt_menu);
 
         bt_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +50,27 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+
+        bt_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonPopupMenu_onClick(view);
+            }
+        });
+
+    }
+
+    private void buttonPopupMenu_onClick(View view) {
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, bt_menu);
+        popupMenu.getMenuInflater().inflate(R.menu.layout_popup_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        popupMenu.show();
     }
 
     private void showFormDialog(){
